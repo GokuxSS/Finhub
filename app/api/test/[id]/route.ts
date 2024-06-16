e779c628d84f297e87f10a3c09077480fa26f581
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-06-16 17:28:18 +0530

    basic api setup with hono

---
+++ app/api/test/[id]/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request:NextRequest,{params}:{params:{id:string}}){
+    return (
+       NextResponse.json({
+        "message":"random",
+        "id":params.id
+       })
+    )
+}
